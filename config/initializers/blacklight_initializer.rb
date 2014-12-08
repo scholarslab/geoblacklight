492f8ef27c52a6fc7690475353059db7e22e5e93
Wayne Graham <wayne.graham@virginia.edu>
2014-12-08 16:17:27 -0500

    Temporarily add secret key

--- config/initializers/blacklight_initializer.rb
+++ config/initializers/blacklight_initializer.rb
@@ -3,5 +3,5 @@
 # will use the application's secret key base instead.
 #
 
-# Blacklight.secret_key = '90da27fba1f81a4a23f083969ad5436b93bb5703d505fbc272bbb82c584299bbfa113fc3dfbe883e88004bfc8f878ddfa1b77d141fa2c6f57ba4876d0207f541'
+Blacklight.secret_key = '90da27fba1f81a4a23f083969ad5436b93bb5703d505fbc272bbb82c584299bbfa113fc3dfbe883e88004bfc8f878ddfa1b77d141fa2c6f57ba4876d0207f541'
 
