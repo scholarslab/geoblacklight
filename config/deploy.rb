af8a7d1922bfa88e7a2908489578aa3884dbcafb
Wayne Graham <wayne.graham@virginia.edu>
2014-12-09 09:37:05 -0500

    Changes for servers

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,20 @@
 # config valid only for current version of Capistrano
 lock '3.3.3'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'geoblacklight'
+set :repo_url, 'https://github.com/scholarslab/geoblacklight.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, "/var/www/#{fetch(:application)}"
 
 # Default value for :scm is :git
 # set :scm, :git
 
 # Default value for :format is :pretty
-# set :format, :pretty
+set :format, :pretty
 
 # Default value for :log_level is :debug
 # set :log_level, :debug
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+#role :app, %w{geoblacklight.lib.virginia.edu}
+#role :web, %w{geoblacklight.lib.virginia.edu}
+#role :db,  %w{geoblacklight.lib.virginia.edu}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'geoblacklight.lib.virginia.edu', user: 'wsg4w', roles: %w{web app db}
 
 
 # Custom SSH Options
